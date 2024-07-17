4e32d9642a08148f93d57c383ea54eb74ea59b6d
yejin <byj729802@gmail.com>
2024-07-17 14:27:57 +0900

    Fix: 공지사항 모달로 띄우기(삭제버튼 추가 필요)

--- src/components/notice/main/detailModal/DetailModal.jsx
+++ src/components/notice/main/detailModal/DetailModal.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import * as SC from './style';
 
-const DetailModal = ({ openDetail, closeDetail, selectedPost }) => {
-  if (!openDetail || !selectedPost) return null;
+const DetailModal = ({ isOpen, closeModal, selectedPost }) => { // prop 이름 수정
+  if (!isOpen || !selectedPost) return null;
 
   return (
-    <SC.DetailModalContainer>
+    <SC.DetailModalContainer isOpen={isOpen}> {/* prop 추가 */}
       <SC.DetailModalContent>
         <SC.DetailModalHeader>
           <SC.DetailTitleText></SC.DetailTitleText>
-          <SC.DetailCloseButton onClick={closeDetail}>&times;</SC.DetailCloseButton>
+          <SC.DetailCloseButton onClick={closeModal}>&times;</SC.DetailCloseButton>
         </SC.DetailModalHeader>
         <SC.DetailModalBody>
           <SC.DetailTitleWrapper>
             <p>{selectedPost.title}</p>
           </SC.DetailTitleWrapper>
+          <SC.Th>{selectedPost.name}</SC.Th>
+          <SC.Th>{selectedPost.date}</SC.Th>
           <SC.DetailContentWrapper>
             <p>{selectedPost.content}</p>
           </SC.DetailContentWrapper>
