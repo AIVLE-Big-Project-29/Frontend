10e8d4550b04f14c61e8b1ca8bad68a723370c32
김태희 <>
2024-07-17 09:45:34 +0900

    Add: 분석 페이지 3d map 및 파일 업로드 추가

--- src/components/Results/MainContent.jsx
+++ src/components/Results/MainContent.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import * as SC from './StyledComponents';
 import Sidebar from './Sidebar/Sidebar';
 import DataRequirementContent from './Frame/Content/DataRequirementContent';
+import IframeMap from './AI/IframeMap';
+import ImageUpload from './AI/ImageUpload';
 
-import * as SC from './StyledComponents'; // import 수정
-
-const Requirement = () => {
+const Results = () => {
   return (
     <SC.Container>
       <SC.SidebarContainer>
         <Sidebar />
       </SC.SidebarContainer>
-      <SC.MainContent>
-        <DataRequirementContent />
+      <SC.MainContent id="capture">
+        {/* <DataRequirementContent /> */}
+        <SC.MapContainer>
+          <SC.Map dangerouslySetInnerHTML={IframeMap()}></SC.Map>
+          {/* <SC.CaptureBtn>
+            <SC.CaptureBtnInner>
+              <SC.CaptureBtnText>Capture</SC.CaptureBtnText>
+            </SC.CaptureBtnInner>
+          </SC.CaptureBtn> */}
+        </SC.MapContainer>
+        <SC.UploadContainer>
+          <ImageUpload />
+        </SC.UploadContainer>
       </SC.MainContent>
     </SC.Container>
   );
 };
 
-export default Requirement;
+export default Results;
