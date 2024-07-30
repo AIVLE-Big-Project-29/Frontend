4dec62929575bd0874915928bbbef75c80eb3a25
김태희 <>
2024-07-30 19:26:18 +0900

    Fix: 로그인 페이지 로고 클릭 기능 추가, 로그아웃 기능 수정

--- src/components/Results/Sidebar/NavSidebar.jsx
+++ src/components/Results/Sidebar/NavSidebar.jsx
@@ -31,6 +31,7 @@ const NavSidebar = () => {
   const handleLogout = () => {
     localStorage.removeItem('accessToken');
     localStorage.removeItem('refreshToken');
+    localStorage.removeItem('userId');
     alert('로그아웃 되었습니다.');
     window.location.href = '/'; // 로그인 페이지로 리다이렉트
   };
--- src/components/login/InnerBlock/InnerBlock.jsx
+++ src/components/login/InnerBlock/InnerBlock.jsx
@@ -95,10 +95,36 @@ const InnerBlock = () => {
     }
   };
 
+  const handleLogin = async () => {
+    // 로고를 클릭하면 테스트 계정으로 로그인하는 함수
+    try {
+      const response = await axios({
+        method: 'post',
+        url: SIGNINURL,
+        data: {
+          username: 'test1234',
+          password: 'test1234',
+        },
+      });
+
+      // 로그인 성공 후 웹 스토리지에 토큰 저장, 메인 페이지로 이동
+      localStorage.setItem('accessToken', response.data['access']);
+      localStorage.setItem('refreshToken', response.data['refresh']);
+      localStorage.setItem('userId', loginInfo.Id);
+
+      navigate('/main');
+    } catch (error) {
+      console.error('로그인 실패', error);
+    }
+  };
+
   return (
     <SC.InnerFrame>
       <SC.Logo>
-        <SC.LogoImg src={logo} alt="logo" />
+        <SC.LogoImg src={logo} alt="logo" onClick={handleLogin} />
+        <SC.TextSmallWrapper>
+          <SC.ExplainText>로고를 누르면 바로 로그인 됩니다</SC.ExplainText>
+        </SC.TextSmallWrapper>
       </SC.Logo>
       <SC.TextWrapper>
         <SC.SignInText>Sign In</SC.SignInText>
